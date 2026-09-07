c96fd8a92df9c3212c676b28ff43f6d14cc3c78e
Wojciech Fornal <wojciech.fornal@gmail.com>
2026-09-07 16:10:06 -0400

    Reset count

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -6,6 +6,11 @@ function increaseCount() {
     checkCountValue();
 }
 
+function resetCount() {
+    count = 0;
+    displayCount();
+}
+
 function displayCount() {
     document.getElementById('countDisplay').innerHTML = count;
 }
