38c2e19370763c605e7eb1ae314d1b75a692e3ab
Wojciech Fornal <wojciech.fornal@gmail.com>
2026-09-12 02:49:50 -0400

    Practice task

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -7,6 +7,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experienceFeedback = document.getElementById('userExperience').value;
 
     alert('Thank you for your valuable feedback');
 
@@ -17,6 +18,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperienceFeedback').innerHTML = experienceFeedback;
 
     document.getElementById('userInfo').style.display = 'block';
 
